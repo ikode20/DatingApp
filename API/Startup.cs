1f779bb2034ca001489feddab751b794fda0ae0c
Ike Okolie <Ike.Okolie@animalfriends.co.uk>
2021-07-06 11:12:49 +0100

    finsish section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -28,7 +28,8 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(options => 
+            services.AddCors();
+            services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
@@ -52,6 +53,13 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(policy =>
+            {
+                policy.AllowAnyHeader()
+                      .AllowAnyMethod()
+                      .WithOrigins("https://localhost:4200");
+            });
 
             app.UseAuthorization();
 
